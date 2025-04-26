2ac8c88c6eabc58afed762bb403f08e02552dd1f
Md Abrar Nasim <mdabrarnasim@192.168.1.3>
2025-04-26 13:58:43 +0530

    adding statice and white noise

--- Instagram-Website/backend/backend/settings.py
+++ Instagram-Website/backend/backend/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 # Middleware
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
