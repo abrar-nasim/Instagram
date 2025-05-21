a7ea163b4b43de69f95cba8acf75a8c05f1d65bd
Md Abrar Nasim <mdabrarnasim@192.168.1.5>
2025-05-21 16:53:25 +0530

    everything working before this commit, will add paypal payment button and flow

--- Instagram-Website/frontend/pages/_app.js
+++ Instagram-Website/frontend/pages/_app.js
@@ -1,12 +1,23 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <script
+          src={`https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID&AW3D4uo3L5NKLTOmbaqGdfVc-NKvjeXfuPAq0_MuOI-w5c-fVb9N4sBS8E7nfe-JWioPXIqA1Xv_RX0D=USD`}
+          data-sdk-integration-source="button-factory"
+        ></script>
+      </Head>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
+
+
